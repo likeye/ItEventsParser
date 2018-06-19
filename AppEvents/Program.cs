f262d3d21cd9c86cdf23c69a1cc75305f0445ccc
Likeye <wozniakdawid.y@gmail.com>
2018-06-19 11:38:59 +0200

    Add project files.

---
+++ AppEvents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EventLibrary_Tests/Events_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLibrary_Tests
+{
+    public class Events_Tests
+    {
+    }
+}
