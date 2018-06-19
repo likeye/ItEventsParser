3622b24a3b96e25f517f81c289836ad4404eded9
Likeye <wozniakdawid.y@gmail.com>
2018-06-19 12:26:28 +0200

    Added Parser class

---
+++ EventLibrary/EventsParserFromHtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace EventLibrary
+{
+    public class EventsParserFromHtml
+    {
+        public void Parser(string city)
+        {
+            var url = @"https://crossweb.pl/wydarzenia/" + city + "/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div[1]/div[2]");
+            var innerText = nodes.Select(node => node.InnerText);
+        }
+    }
+}
