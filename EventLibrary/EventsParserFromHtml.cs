cd659f42aba33f0891892e5e96b30eb8a5ac489f
Likeye <wozniakdawid.y@gmail.com>
2018-06-19 19:30:05 +0200

    Added some changes in parser class

--- EventLibrary/EventsParserFromHtml.cs
+++ EventLibrary/EventsParserFromHtml.cs
@@ -14,8 +14,20 @@ namespace EventLibrary
             var url = @"https://crossweb.pl/wydarzenia/" + city + "/";
             var web = new HtmlWeb();
             var doc = web.Load(url);
-            var nodes = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div");
-            var innerTexts = nodes.Select(node => node.InnerText);
+            var nodeNames = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div/a/div[@class=\"colTab title\"]"); //to i 
+            var nodeDatas = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div/a/div/span[@class=\"colDataDay\"]");
+            var nodeDescriptions = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div/a/div[@class=\"colTab topic phoneOff\"]"); // to teoretycznie jest so samo (?)
+            var nodeLinks = doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div/a");
+            //foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//*[@id=\"container\"]/div/div/a/div/span") )
+            //{
+            //    var text = node.Attributes[]
+            //}
+
+            var innerTextNames = nodeNames.Select(node => node.InnerText);
+            var innerTextDatas = nodeDatas.Select(node => node.InnerText);
+            var innerTextDescriptions = nodeDescriptions.Select(node => node.InnerText);
+            var innerTextLinks = nodeLinks.Select(node => node.InnerText);
+            
         }
     }
 }
