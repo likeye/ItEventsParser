d93c8f196ee9f306d175b73f213ac1296868c011
Likeye <wozniakdawid.y@gmail.com>
2018-06-19 19:46:36 +0200

    added line in class connectionDataBase

--- EventLibrary/ConnectToDataBase.cs
+++ EventLibrary/ConnectToDataBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace EventLibrary
 {
     public class ConnectToDataBase
     {
+        private void Conection()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server= []; Database = []; Trusted_Connection = true";
+
+            }
+        }
     }
 }
